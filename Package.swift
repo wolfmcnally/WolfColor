7c57c629d9554c994d5c89a93ab17d922f0f5522
Wolf McNally <wolf@wolfmcnally.com>
2020-12-20 22:47:37 -0800

    Stopped being dynamic.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
     products: [
         .library(
             name: "WolfColor",
-            type: .dynamic,
             targets: ["WolfColor"]),
     ],
     dependencies: [
